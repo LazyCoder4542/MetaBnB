7412d98e6233c1ff2e3b2ece2863b7cc436f61d8
Lazy Coder <adeolafadare8@gmail.com>
2022-11-11 17:33:44 +0100

    feat: added header jsx code imported fonts and added general styling

--- react-app/src/components/molecules/Header.jsx
+++ react-app/src/components/molecules/Header.jsx
@@ -1,7 +1,29 @@
 import React from 'react';
 
+import { ReactComponent as Logo} from './../../assets/icons/logo.svg'
 function Header() {
-    return ( <></> );
+    return (
+        <header id="site-header">
+            <div className="logo">
+                <Logo />
+            </div>
+            <div className="menu">
+                <div className="wrapper">
+                    <div className="links">
+                        <ul>
+                            <li>Home</li>
+                            <li>Place to stay</li>
+                            <li>NFTs</li>
+                            <li>Community</li>
+                        </ul>
+                    </div>
+                    <div className="call-to-action">
+                        <div className="btn">Connect wallet</div>
+                    </div>
+                </div>
+            </div>
+        </header>
+    );
 }
 
 export default Header;
