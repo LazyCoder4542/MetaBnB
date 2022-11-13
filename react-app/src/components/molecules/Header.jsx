a07664a0c65ef2a6787815da7245f63b267bc6b3
Lazy Coder <adeolafadare8@gmail.com>
2022-11-13 13:50:30 +0100

    feat: added menu for lower screen sizes

--- react-app/src/components/molecules/Header.jsx
+++ react-app/src/components/molecules/Header.jsx
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
 import { Link, NavLink } from 'react-router-dom';
 import ConnectWallet from '../atoms/ConnectWallet';
 import { ReactComponent as Logo} from './../../assets/icons/logo.svg'
 function Header() {
+    const [isMenuOpen, setIsMenuOpen] = useState(false)
     return (
         <header id="site-header">
             <div className="logo">
@@ -11,7 +12,17 @@ function Header() {
                     <Logo />
                 </Link>
             </div>
-            <div className="menu">
+            <div className="menu" data-isopen={isMenuOpen}>
+                <div
+                className="hamburger"
+                onClick={()=> {
+                    setIsMenuOpen(!isMenuOpen)
+                }}
+                >
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>
                 <div className="wrapper">
                     <div className="links">
                         <ul>
@@ -21,16 +32,16 @@ function Header() {
                             <li>Community</li>
                         </ul>
                     </div>
-                    <div className="call-to-action">
-                        <div
-                        className="btn btn-primary"
-                        onClick={()=> {
-                            const smth = ReactDOM.createRoot(document.getElementById('a'))
-                            smth.render(<ConnectWallet root={smth} />)
-                        }}
-                        >Connect wallet</div>
-                    </div>
                 </div>
+            </div>
+            <div className="call-to-action">
+                <div
+                className="btn btn-primary"
+                onClick={()=> {
+                    const smth = ReactDOM.createRoot(document.getElementById('a'))
+                    smth.render(<ConnectWallet root={smth} />)
+                }}
+                >Connect wallet</div>
             </div>
         </header>
     );
